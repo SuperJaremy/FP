b81753407dcdacc7a830f438aae80fb0e64df167
SuperJaremy <legoitcomp@gmail.com>
2022-11-19 22:56:16 +0300

    Problem 15 recursion

---
+++ src/FP/Fifteen.fs
@@ -0,0 +1,16 @@
+﻿namespace FP.Fifteen
+module Recursion =
+    let rec private countPaths_in (point : bigint, matrixSide : bigint) =
+        if point = bigint.Zero then
+            bigint.One
+        elif (point < matrixSide + bigint.One) then
+            countPaths_in ((point - bigint.One), matrixSide)
+        elif (point % (matrixSide + bigint.One) = bigint.Zero) then
+            countPaths_in ((point - matrixSide - bigint.One), matrixSide)
+        else
+            (countPaths_in ((point - bigint.One, matrixSide))
+            + (countPaths_in ((point - (matrixSide + bigint.One)), matrixSide)))
+
+    let countPaths matrixSide:bigint = countPaths_in (((matrixSide + bigint.One) * (matrixSide + bigint.One) - bigint.One), matrixSide)
+    
+    // printfn "The answer is %A" (countPaths 15I)
--- tests/FP.Tests/UnitTest1.fs
+++ tests/FP.Tests/UnitTest1.fs
@@ -1,9 +1,11 @@
 module FP.Tests
 
+open FP.Fifteen
 open NUnit.Framework
 
 [<SetUp>]
 let Setup () = ()
 
 [<Test>]
-let Test1 () = Assert.Pass()
+let Test1 () = Assert.AreEqual(6I, Recursion.countPaths 2I)
+
