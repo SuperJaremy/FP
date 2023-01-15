d1945502d8eb04cd5e9b18e07884de8e0233cef4
SuperJaremy <legoitcomp@gmail.com>
2023-01-15 22:03:41 +0500

    Add some unit tests

--- tests/FP.Tests/UnitTest1.fs
+++ tests/FP.Tests/UnitTest1.fs
@@ -1,9 +1,53 @@
 module FP.Tests
 
 open NUnit.Framework
-
-[<SetUp>]
-let Setup () = ()
+open AVLTree
 
 [<Test>]
-let Test1 () = Assert.Pass()
+let TestInsertToEmpty () =
+    Assert.AreEqual(T(E, 5, E), (insert 5 E))
+
+[<Test>]
+let TestInsert () =
+    Assert.AreEqual(T(T(E, 3, E), 5, E), T(E, 5, E) |> insert 3)
+
+[<Test>]
+let TestLeftBalancing () =
+    Assert.AreEqual(T(T(E, 10, E), 10, T(E, 10, E)), insert 10 E |> insert 10 |> insert 10)
+
+[<Test>]
+let TestRightBalancing () =
+    Assert.AreEqual(T(T(E, 1, E), 2, T(E, 3, E)), insert 3 E |> insert 2 |> insert 1)
+
+[<Test>]
+let TestDeleteFromEmpty () =
+    let tree = E :> AVLTree<int>
+    Assert.AreEqual(tree, delete 100 E)
+
+[<Test>]
+let TestDeleteValueNotPresentInTree () =
+    let tree = T(T(E, 1, E), 2, T(E, 3, E))
+    Assert.AreEqual(tree, delete 100 tree)
+
+[<Test>]
+let TestDelete () =
+    let tree = T(T(E, 1, E), 2, T(E, 3, E))
+    Assert.AreEqual(T(E, 2, T(E, 3, E)), delete 1 tree)
+
+[<Test>]
+let TestAVLLeftRotation () =
+    let tree = T(T(E, 40, E), 50, T(T(E, 55, E), 60, T(E, 70, E)))
+
+    let treeAfterRotation =
+        T(T(T(E, 40, E), 50, T(E, 52, E)), 55, T(E, 60, T(E, 70, E)))
+
+    Assert.AreEqual(treeAfterRotation, insert 52 tree)
+
+[<Test>]
+let TestAVLRightRotation () =
+    let tree = T(T(T(E, 30, E), 40, T(E, 45, E)), 50, T(E, 60, E))
+
+    let treeAfterRotation =
+        T(T(T(E, 30, E), 40, E), 45, T(T(E, 47, E), 50, T(E, 60, E)))
+
+    Assert.AreEqual(treeAfterRotation, insert 47 tree)
