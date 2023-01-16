de93e279193f3d84c2ca66abc833e34229703f8c
SuperJaremy <legoitcomp@gmail.com>
2023-01-16 11:44:41 +0300

    Fix format

--- tests/FP.Tests/UnitTest1.fs
+++ tests/FP.Tests/UnitTest1.fs
@@ -38,7 +38,8 @@ let TestDelete () =
 let TestAVLLeftRotation () =
     let tree = T(T(E, 40, E), 50, T(T(E, 55, E), 60, T(E, 70, E)))
 
-    let treeAfterRotation = T(T(T(E, 40, E), 50, T(E, 52, E)), 55, T(E, 60, T(E, 70, E)))
+    let treeAfterRotation =
+        T(T(T(E, 40, E), 50, T(E, 52, E)), 55, T(E, 60, T(E, 70, E)))
 
     Assert.AreEqual(treeAfterRotation, insert 52 tree)
 
@@ -46,7 +47,8 @@ let TestAVLLeftRotation () =
 let TestAVLRightRotation () =
     let tree = T(T(T(E, 30, E), 40, T(E, 45, E)), 50, T(E, 60, E))
 
-    let treeAfterRotation = T(T(T(E, 30, E), 40, E), 45, T(T(E, 47, E), 50, T(E, 60, E)))
+    let treeAfterRotation =
+        T(T(T(E, 30, E), 40, E), 45, T(T(E, 47, E), 50, T(E, 60, E)))
 
     Assert.AreEqual(treeAfterRotation, insert 47 tree)
 
@@ -60,7 +62,10 @@ let TestFilter () =
 let TestMap () =
     let tree = T(T(T(E, 4, E), 1, T(E, 13, E)), 20, T(T(E, 18, E), 9, T(E, 7, E)))
     let f = (*) 2
-    let treeAfterMap = T(T(T(E, 8, E), 2, T(E, 26, E)), 40, T(T(E, 36, E), 18, T(E, 14, E)))
+
+    let treeAfterMap =
+        T(T(T(E, 8, E), 2, T(E, 26, E)), 40, T(T(E, 36, E), 18, T(E, 14, E)))
+
     Assert.AreEqual(treeAfterMap, (tree |> map f))
 
 [<Test>]
@@ -79,5 +84,8 @@ let TestRightFold () =
 let TestConcat () =
     let tree1 = T(T(E, 1, E), 2, T(E, 3, E))
     let tree2 = T(T(T(E, 3, E), 4, E), 5, T(E, 6, E))
-    let treeAfterConcat = T(T(T(E, 1, E), 2, E), 3, T(T(T(E, 3, E), 4, E), 5, T(E, 6, E)))
+
+    let treeAfterConcat =
+        T(T(T(E, 1, E), 2, E), 3, T(T(T(E, 3, E), 4, E), 5, T(E, 6, E)))
+
     Assert.AreEqual(treeAfterConcat, concat tree1 tree2)
