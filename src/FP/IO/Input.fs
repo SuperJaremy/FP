214137dc6a4f3bb404179bff6585ae14622ab6c6
SuperJaremy <legoitcomp@gmail.com>
2023-01-26 22:59:11 +0300

    Read dots from stdin

--- src/FP/IO/Input.fs
+++ src/FP/IO/Input.fs
@@ -1,7 +1,21 @@
 ﻿module Input
-    let mutable state = (0.0,1.0)
+
+    open System
+
+    let parseFloat s = try Some (float s) with | _ -> None
+    
+    let readTwoNumbersFromStdin () =
+        let line = Console.ReadLine()
+        let values = line.Split '\t'
+        if values.Length = 2 then
+            let (x, y) = (parseFloat values[0], parseFloat values[1])
+            match x,y with
+            | Some a, Some b -> (a, b)
+            | _ -> FormatException("Wrong input format") |> raise
+        else
+            FormatException("Wrong input format") |> raise
+    
     let readNewDot () =
-        let (x, y) = state
-        state <- (x + 1.0, y)
-        state
+        let dot = readTwoNumbersFromStdin()
+        dot
 
